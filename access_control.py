c97a3a7c7422206d7ccc9cba50ffb2b2283261fe
srijanstha619-rgb <srijanstha619@gmail.com>
2026-02-18 23:16:47 +0545

    Initial commit: add access_control.py

---
+++ access_control.py
@@ -0,0 +1,45 @@
+#Access Control System
+
+print("ACCESS CONTROL SYSTEM")
+
+u = "admin"
+p = "1234"
+
+count = 0
+
+while count < 3:
+
+    name = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if name == u:
+        if password == p:
+
+            print("Login Successful")
+            print("Access Granted")
+
+            print("1. View Data")
+            print("2. Exit")
+
+            option = input("Enter choice: ")
+
+            if option == "1":
+                print("You can see the secret data.")
+            elif option == "2":
+                print("Exit Program")
+            else:
+                print("Wrong Choice")
+
+            break
+
+        else:
+            print("Wrong Password")
+
+    else:
+        print("Wrong Username")
+
+    count = count + 1
+    print("Attempts left:", 3 - count)
+
+if count == 3:
+    print("Access Denied")
